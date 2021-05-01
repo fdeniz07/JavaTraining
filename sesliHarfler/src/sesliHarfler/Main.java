16965bbbebbb4e7c5afe1235399fa30834d0e924
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-01 11:42:48 +0200

    Sesli harfler

---
+++ sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,22 @@
+package sesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char harf='E';
+		
+		switch (harf) {
+		case 'A': 
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalin sesli harf");
+		break;
+		default:
+			System.out.println("Ince sesli harf");
+		}
+		
+
+	}
+
+}
