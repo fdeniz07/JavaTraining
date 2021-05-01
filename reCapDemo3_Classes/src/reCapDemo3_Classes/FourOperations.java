35ff25c2a876fe9aafe685d3bf752b7501ffdc33
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-01 11:42:23 +0200

    ReCap Demo3

---
+++ reCapDemo3_Classes/src/reCapDemo3_Classes/FourOperations.java
@@ -0,0 +1,19 @@
+package reCapDemo3_Classes;
+
+public class FourOperations {
+	public int Addition(int number1, int number2) {
+		return number1 + number2;
+	}
+	
+	public int Subtraction(int number1, int number2) {
+		return number1 - number2;
+	}
+	
+	public int Multiplication(int number1, int number2) {
+		return number1 * number2;
+	}
+	
+	public int Division(int number1, int number2) {
+		return number1 / number2;
+	}
+}
---
+++ reCapDemo3_Classes/src/reCapDemo3_Classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo3_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		FourOperations fourOperations = new FourOperations();
+		int result = fourOperations.Addition(3, 4);
+		System.out.println(result);
+	}
+
+}
