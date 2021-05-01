0e60fec57c4d91fcdf837bbb0f8d8817cd521d4e
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-01 11:39:22 +0200

    loop demo

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,30 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// For
+		for (int i = 1; i < 10; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println("For döngüsü bitti");
+
+		// While
+		int i = 1;
+		while (i < 10) {
+			System.out.println(i);
+			i += 2;
+		}
+		System.out.println("While döngüsü bitti");
+
+		// Do-While
+		int j = 1;
+		do {
+			System.out.println(j);
+			j += 2;
+		} while (j < 10);
+
+		System.out.println("Do-While döngüsü bitti");
+	}
+
+}
