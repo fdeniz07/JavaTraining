6762d4fb5a8ce80443b74fb4e242e0de899b1c9b
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-01 11:37:50 +0200

    conditionals

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,20 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Conditionals
+
+		int number = 20;
+
+		if (number < 20) {
+			System.out.println("Number is smaller than 20");
+		} else if (number == 20) {
+			System.out.println("Number is equals 20");
+		} else {
+			System.out.println("Number is bigger than 20");
+		}
+
+	}
+
+}
