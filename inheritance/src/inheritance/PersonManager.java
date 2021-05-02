87a6c2b8463e5c653887109f80fbcacead248b87
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-03 01:16:31 +0200

    Inheritance

---
+++ inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class PersonManager {
+	public void List() {
+		System.out.println("Person listed");
+	}
+	
+	public void Add() {
+		System.out.println("Person added");
+	}
+}
