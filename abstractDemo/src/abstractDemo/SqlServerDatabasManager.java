61fdcb6487c9e4dc55e498a72aca7878e7d00dbb
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-04 16:30:16 +0200

    abstractDemo

---
+++ abstractDemo/src/abstractDemo/SqlServerDatabasManager.java
@@ -0,0 +1,11 @@
+package abstractDemo;
+
+public class SqlServerDatabasManager extends BaseDatabaseManager{
+
+	@Override
+	public void getData() {
+		System.out.println("Data called : Sql Server");
+		
+	}
+
+}
